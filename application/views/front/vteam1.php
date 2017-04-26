7859419422e3f646294b27e5b40fae41ea86185d
ndragoonz <fadilndras@gmail.com>
2017-04-26 22:27:37 +0700

    trim view

--- application/views/front/vteam1.php
+++ application/views/front/vteam1.php
@@ -1,400 +1,232 @@
 <?php $this->load->view('front/includes/header') ?>
-
         <div id="content">
-
-
             <div id="page-title">
-
-
                 <div class="container clearfix">
-
                     <h1>SoftDev Team<span>/ The Core of our Company</span></h1>
-
                     <div id="top-search">
-
                         <form id="top-search-form" action="search-results.php" method="get">
-
                             <input type="text" id="top-search-input" name="q" value="" placeholder="Type &amp; Hit Enter" />
-
                         </form>
-
                     </div>
-
                 </div>
-
-
             </div>
 
-
             <div class="content-wrap">
-
-
                 <div class="container clearfix">
-
-
                     <div class="col_two_third entry_content">
-
                         <h3>Our <span>Amazing</span> Team</h3>
-
                         <div class="dropcap">S</div><p>ed nunc tortor, sagittis quis viverra id, molestie vitae nibh. Donec vitae lacus sed risus accumsan consectetur ut a ligula. Etiam vitae nisi diam, in rutrum dui. In facilisis, erat sed laoreet aliquet, mauris urna placerat enim, eget varius nulla lacus ut erat.</p>
-
-
                         <h4>We are...</h4>
-
                         <div class="col_half nobottommargin">
-
                             <ul class="fa-ul">
-
                                 <li><i class="fa fa-li fa-star"></i>Very Professional</li>
                                 <li><i class="fa fa-li fa-star"></i>Hard Working</li>
                                 <li><i class="fa fa-li fa-star"></i>Punctual Gentlemen</li>
-
                             </ul>
-
                         </div>
-
                         <div class="col_half col_last nobottommargin">
-
                             <ul class="fa-ul">
-
                                 <li><i class="fa fa-li fa-star"></i>Polite &amp; Friendly</li>
                                 <li><i class="fa fa-li fa-star"></i>Intellectual Minds</li>
                                 <li><i class="fa fa-li fa-star"></i>Efficiently Equipped</li>
-
                             </ul>
-
                         </div>
-
                     </div>
 
 
                     <div class="col_one_third col_last">
-
                         <div class="well callout">
-
                             <h2>We're <span>Hiring</span>!</h2>
-
                             <p class="muted">Donec vitae lacus sed risus accumsan consectetur ut a ligula. Etiam vitae nisi diam, in rutrum dui eget varius nulla lacus ut erat.</p>
-
                             <a href="#" class="simple-button nomargin">Apply Now</a>
-
                         </div>
-
                     </div>
-
 
                     <div class="clear"></div>
 
-
                     <div class="col_one_third clearfix">
-
-
                         <div class="team-member">
-
                             <div class="team-image">
-
                                 <img src="<?= base_url() ?>assets/front_assets/images/staff/1.jpg" alt="Team Member" title="Team Member" />
-
                                 <span>CEO, Director</span>
-
                             </div>
 
                             <div class="team-desc">
-
                                 <h4>John Doe</h4>
-
                                 <p>Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh vehicula ut id elit.</p>
-
                                 <div class="team-social">
-
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/facebook.png" class="ntip" alt="Facebook" title="Facebook" /></a>
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/twitter.png" class="ntip" alt="Twitter" title="Twitter" /></a>
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/dribbble.png" class="ntip" alt="Dribbble" title="Dribbble" /></a>
-
                                 </div>
-
                             </div>
 
                             <ul class="team-skills">
-
                                 <li><span class="fa fa-check"></span>HTML5</li>
                                 <li><span class="fa fa-check"></span>PHP</li>
                                 <li><span class="fa fa-check"></span>AJAX</li>
-
                             </ul>
-
                         </div>
-
-
                     </div>
 
 
                     <div class="col_one_third clearfix">
-
-
                         <div class="team-member">
-
                             <div class="team-image">
-
                                 <img src="<?= base_url() ?>assets/front_assets/images/staff/2.jpg" alt="Team Member" title="Team Member" />
-
                                 <span>CFO, Finance Manager</span>
-
                             </div>
 
                             <div class="team-desc">
-
                                 <h4>Jeremy Clarke</h4>
-
                                 <p>Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh vehicula ut id elit.</p>
-
                                 <div class="team-social">
-
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/facebook.png" class="ntip" alt="Facebook" title="Facebook" /></a>
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/twitter.png" class="ntip" alt="Twitter" title="Twitter" /></a>
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/forrst.png" class="ntip" alt="Forrst" title="Forrst" /></a>
-
                                 </div>
-
                             </div>
 
                             <ul class="team-skills">
-
                                 <li><span class="fa fa-check"></span>HTML5</li>
                                 <li><span class="fa fa-check"></span>AJAX</li>
-
                             </ul>
-
                         </div>
-
-
                     </div>
 
 
                     <div class="col_one_third col_last clearfix">
-
-
                         <div class="team-member">
-
                             <div class="team-image">
-
                                 <img src="<?= base_url() ?>assets/front_assets/images/staff/3.jpg" alt="Team Member" title="Team Member" />
-
                                 <span>Support Staff</span>
-
                             </div>
 
                             <div class="team-desc">
-
                                 <h4>Ricky Ponting</h4>
-
                                 <p>Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh vehicula ut id elit.</p>
-
                                 <div class="team-social">
-
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/facebook.png" class="ntip" alt="Facebook" title="Facebook" /></a>
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/flickr.png" class="ntip" alt="Flickr" title="Flickr" /></a>
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/forrst.png" class="ntip" alt="Forrst" title="Forrst" /></a>
-
                                 </div>
-
                             </div>
 
                             <ul class="team-skills">
-
                                 <li><span class="fa fa-check"></span>HTML5</li>
                                 <li><span class="fa fa-check"></span>PHP</li>
                                 <li><span class="fa fa-check"></span>CSS3</li>
                                 <li><span class="fa fa-check"></span>AJAX</li>
-
                             </ul>
-
                         </div>
-
-
                     </div>
-
 
                     <div class="clear"></div>
 
-
                     <div class="col_one_fourth nobottommargin clearfix">
-
-
                         <div class="team-member team-rounded">
-
                             <div class="team-image">
-
                                 <img src="<?= base_url() ?>assets/front_assets/images/staff/1-2.jpg" alt="Team Member" title="Team Member" />
-
                                 <span>CEO, Director</span>
-
                             </div>
 
                             <div class="team-desc">
-
                                 <h4>John Doe</h4>
-
                                 <p>Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh vehicula ut id elit.</p>
-
                                 <div class="team-social hidden-tablet">
-
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/facebook.png" class="ntip" alt="Facebook" title="Facebook" /></a>
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/twitter.png" class="ntip" alt="Twitter" title="Twitter" /></a>
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/dribbble.png" class="ntip" alt="Dribbble" title="Dribbble" /></a>
-
                                 </div>
 
                             </div>
 
                             <ul class="team-skills">
-
                                 <li><span class="fa fa-check"></span>HTML5</li>
                                 <li><span class="fa fa-check"></span>PHP</li>
                                 <li><span class="fa fa-check"></span>AJAX</li>
-
                             </ul>
-
                         </div>
-
-
                     </div>
-
 
                     <div class="col_one_fourth nobottommargin clearfix">
 
-
                         <div class="team-member team-rounded">
-
                             <div class="team-image">
-
                                 <img src="<?= base_url() ?>assets/front_assets/images/staff/2-2.jpg" alt="Team Member" title="Team Member" />
-
                                 <span>CFO, Financer</span>
-
                             </div>
 
                             <div class="team-desc">
-
                                 <h4>Jeremy Clarke</h4>
-
                                 <p>Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh vehicula ut id elit.</p>
-
                                 <div class="team-social hidden-tablet">
-
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/facebook.png" class="ntip" alt="Facebook" title="Facebook" /></a>
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/twitter.png" class="ntip" alt="Twitter" title="Twitter" /></a>
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/forrst.png" class="ntip" alt="Forrst" title="Forrst" /></a>
-
                                 </div>
-
                             </div>
 
                             <ul class="team-skills">
-
                                 <li><span class="fa fa-check"></span>HTML5</li>
                                 <li><span class="fa fa-check"></span>AJAX</li>
-
                             </ul>
-
                         </div>
-
-
                     </div>
 
 
                     <div class="col_one_fourth nobottommargin clearfix">
-
-
                         <div class="team-member team-rounded">
-
                             <div class="team-image">
-
                                 <img src="<?= base_url() ?>assets/front_assets/images/staff/3-2.jpg" alt="Team Member" title="Team Member" />
-
                                 <span>Support Staff</span>
-
                             </div>
 
                             <div class="team-desc">
-
                                 <h4>Ricky Ponting</h4>
-
                                 <p>Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh vehicula ut id elit.</p>
-
                                 <div class="team-social hidden-tablet">
-
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/facebook.png" class="ntip" alt="Facebook" title="Facebook" /></a>
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/flickr.png" class="ntip" alt="Flickr" title="Flickr" /></a>
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/forrst.png" class="ntip" alt="Forrst" title="Forrst" /></a>
-
                                 </div>
-
                             </div>
 
                             <ul class="team-skills">
-
                                 <li><span class="fa fa-check"></span>HTML5</li>
                                 <li><span class="fa fa-check"></span>PHP</li>
                                 <li><span class="fa fa-check"></span>CSS3</li>
-
                             </ul>
-
                         </div>
-
-
                     </div>
 
 
                     <div class="col_one_fourth nobottommargin col_last clearfix">
-
-
                         <div class="team-member team-rounded">
-
                             <div class="team-image">
-
                                 <img src="<?= base_url() ?>assets/front_assets/images/staff/5-2.jpg" alt="Team Member" title="Team Member" />
-
                                 <span>Support Staff</span>
-
                             </div>
 
                             <div class="team-desc">
-
                                 <h4>Ricky Ponting</h4>
-
                                 <p>Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh vehicula ut id elit.</p>
-
                                 <div class="team-social hidden-tablet">
-
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/facebook.png" class="ntip" alt="Facebook" title="Facebook" /></a>
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/flickr.png" class="ntip" alt="Flickr" title="Flickr" /></a>
                                     <a href="#"><img src="<?= base_url() ?>assets/front_assets/images/staff/social/forrst.png" class="ntip" alt="Forrst" title="Forrst" /></a>
-
                                 </div>
-
                             </div>
 
                             <ul class="team-skills">
-
                                 <li><span class="fa fa-check"></span>HTML5</li>
                                 <li><span class="fa fa-check"></span>PHP</li>
                                 <li><span class="fa fa-check"></span>AJAX</li>
-
                             </ul>
-
                         </div>
-
-
                     </div>
-
-
                 </div>
-
-
             </div>
-
-
         </div>
 
-<?php $this->load->view('front/includes/footer') ?>  
+<?php $this->load->view('front/includes/footer') ?>
